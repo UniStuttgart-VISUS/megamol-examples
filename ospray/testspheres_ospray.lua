8d0b4fca8efae54771c0ae0ae9b8a46d65629785
Matthias Braun <matthias.braun@visus.uni-stuttgart.de>
2021-07-06 12:11:05 +0200

    adjusted ospray examples

--- ospray/testspheres_ospray.lua
+++ ospray/testspheres_ospray.lua
@@ -1,7 +1,7 @@
 
 mmCreateView("GraphEntry_1","View3DGL","::View3DGL_1") 
 
-mmCreateModule("ContextToGL","::ContextToGL_1") 
+mmCreateModule("OSPRayToGL","::OSPRayToGL_1") 
 mmCreateModule("OSPRayRenderer","::OSPRayRenderer_1") 
 mmCreateModule("BoundingBoxRenderer","::BoundingBoxRenderer_1") 
 mmCreateModule("AmbientLight","::AmbientLight_1") 
@@ -10,10 +10,10 @@ mmCreateModule("OSPRayOBJMaterial","::OSPRayOBJMaterial_1")
 mmCreateModule("TestSpheresDataSource","::TestSpheresDataSource_1") 
 
 mmCreateCall("CallRender3DGL","::View3DGL_1::rendering","::BoundingBoxRenderer_1::rendering")
-mmCreateCall("CallRender3D","::ContextToGL_1::getContext","::OSPRayRenderer_1::rendering")
+mmCreateCall("CallRender3D","::OSPRayToGL_1::getContext","::OSPRayRenderer_1::rendering")
 mmCreateCall("CallLight","::OSPRayRenderer_1::lights","::AmbientLight_1::deployLightSlot")
 mmCreateCall("CallOSPRayStructure","::OSPRayRenderer_1::getStructure","::OSPRaySphereGeometry_1::deployStructureSlot")
-mmCreateCall("CallRender3DGL","::BoundingBoxRenderer_1::chainRendering","::ContextToGL_1::rendering")
+mmCreateCall("CallRender3DGL","::BoundingBoxRenderer_1::chainRendering","::OSPRayToGL_1::rendering")
 mmCreateCall("CallOSPRayMaterial","::OSPRaySphereGeometry_1::getMaterialSlot","::OSPRayOBJMaterial_1::deployMaterialSlot")
 mmCreateCall("MultiParticleDataCall","::OSPRaySphereGeometry_1::getdata","::TestSpheresDataSource_1::getData")
 
