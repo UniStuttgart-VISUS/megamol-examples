245ae117542df697ebb2a3b2e766a75546370d3b
Moritz Heinemann <moritz.heinemann@visus.uni-stuttgart.de>
2023-03-13 15:26:49 +0100

    colored spheres

--- infovis-spheres.lua
+++ infovis-spheres.lua
@@ -8,12 +8,13 @@ mmCreateModule("View2DGL","::View2DGL_2")
 mmCreateModule("View2DGL","::View2DGL_3")
 mmCreateModule("SphereRenderer","::SphereRenderer_1")
 mmCreateModule("TableToParticles","::TableToParticles_1")
+mmCreateModule("TransferFunctionGL","::TransferFunctionGL_1")
 mmCreateModule("DistantLight","::DistantLight_1")
 mmCreateModule("ParallelCoordinatesRenderer2D","::ParallelCoordinatesRenderer2D_1")
 mmCreateModule("ScatterplotMatrixRenderer2D","::ScatterplotMatrixRenderer2D_1")
 mmCreateModule("TableHistogramRenderer2D","::TableHistogramRenderer2D_1")
 mmCreateModule("CSVDataSource","::CSVDataSource_1")
-mmCreateModule("TransferFunctionGL","::TransferFunction_1")
+mmCreateModule("TransferFunctionGL","::TransferFunctionGL_2")
 mmCreateModule("UniFlagStorageGL","::UniFlagStorageGL_1")
 
 mmCreateCall("CallRenderViewGL","::SplitViewGL_1::render1","::SplitViewGL_2::render")
@@ -27,26 +28,26 @@ mmCreateCall("CallRender2DGL","::View2DGL_1::rendering","::ParallelCoordinatesRe
 mmCreateCall("CallRender2DGL","::View2DGL_2::rendering","::ScatterplotMatrixRenderer2D_1::rendering")
 mmCreateCall("CallRender2DGL","::View2DGL_3::rendering","::TableHistogramRenderer2D_1::rendering")
 mmCreateCall("MultiParticleDataCall","::SphereRenderer_1::getdata","::TableToParticles_1::multidata")
-mmCreateCall("CallGetTransferFunctionGL","::SphereRenderer_1::gettransferfunction","::TransferFunction_1::gettransferfunction")
+mmCreateCall("CallGetTransferFunctionGL","::SphereRenderer_1::gettransferfunction","::TransferFunctionGL_1::gettransferfunction")
 mmCreateCall("CallLight","::SphereRenderer_1::lights","::DistantLight_1::deployLightSlot")
 mmCreateCall("FlagCallRead_GL","::SphereRenderer_1::readFlags","::UniFlagStorageGL_1::readFlags")
 mmCreateCall("TableDataCall","::TableToParticles_1::floattable","::CSVDataSource_1::getData")
 mmCreateCall("TableDataCall","::ParallelCoordinatesRenderer2D_1::getData","::CSVDataSource_1::getData")
-mmCreateCall("CallGetTransferFunctionGL","::ParallelCoordinatesRenderer2D_1::getTransferFunction","::TransferFunction_1::gettransferfunction")
+mmCreateCall("CallGetTransferFunctionGL","::ParallelCoordinatesRenderer2D_1::getTransferFunction","::TransferFunctionGL_2::gettransferfunction")
 mmCreateCall("FlagCallRead_GL","::ParallelCoordinatesRenderer2D_1::readFlagStorage","::UniFlagStorageGL_1::readFlags")
 mmCreateCall("FlagCallWrite_GL","::ParallelCoordinatesRenderer2D_1::writeFlagStorage","::UniFlagStorageGL_1::writeFlags")
 mmCreateCall("TableDataCall","::ScatterplotMatrixRenderer2D_1::ftIn","::CSVDataSource_1::getData")
-mmCreateCall("CallGetTransferFunctionGL","::ScatterplotMatrixRenderer2D_1::tfIn","::TransferFunction_1::gettransferfunction")
+mmCreateCall("CallGetTransferFunctionGL","::ScatterplotMatrixRenderer2D_1::tfIn","::TransferFunctionGL_2::gettransferfunction")
 mmCreateCall("FlagCallRead_GL","::ScatterplotMatrixRenderer2D_1::readFlags","::UniFlagStorageGL_1::readFlags")
 mmCreateCall("FlagCallWrite_GL","::ScatterplotMatrixRenderer2D_1::writeFlags","::UniFlagStorageGL_1::writeFlags")
-mmCreateCall("CallGetTransferFunctionGL","::TableHistogramRenderer2D_1::getTransferFunction","::TransferFunction_1::gettransferfunction")
+mmCreateCall("CallGetTransferFunctionGL","::TableHistogramRenderer2D_1::getTransferFunction","::TransferFunctionGL_2::gettransferfunction")
 mmCreateCall("TableDataCall","::TableHistogramRenderer2D_1::getData","::CSVDataSource_1::getData")
 mmCreateCall("FlagCallRead_GL","::TableHistogramRenderer2D_1::readFlagStorage","::UniFlagStorageGL_1::readFlags")
 mmCreateCall("FlagCallWrite_GL","::TableHistogramRenderer2D_1::writeFlagStorage","::UniFlagStorageGL_1::writeFlags")
 
 mmSetParamValue("::SplitViewGL_1::split.orientation",[=[Vertical]=])
-mmSetParamValue("::TableToParticles_1::color",[=[white]=])
-mmSetParamValue("::TableToParticles_1::colormode",[=[global RGB]=])
+mmSetParamValue("::TableToParticles_1::intensitycolumnname",[=[species]=])
+mmSetParamValue("::TableToParticles_1::colormode",[=[Intensity]=])
 mmSetParamValue("::TableToParticles_1::radius",[=[0.100000]=])
 mmSetParamValue("::TableToParticles_1::radiusmode",[=[global]=])
 mmSetParamValue("::TableToParticles_1::xcolumnname",[=[sepal_length]=])
@@ -55,3 +56,18 @@ mmSetParamValue("::TableToParticles_1::zcolumnname",[=[petal_length]=])
 mmSetParamValue("::CSVDataSource_1::filename",[=[../examples/sampledata/iris.csv]=])
 mmSetParamValue("::CSVDataSource_1::colSep",[=[,]=])
 mmSetParamValue("::CSVDataSource_1::decSep",[=[US (3.141)]=])
+mmSetParamValue("::TransferFunctionGL_1::textureInterpolation",[=[Nearest neighbor]=])
+mmSetParamValue("::TransferFunctionGL_1::TransferFunction",[=[{
+  "IgnoreProjectRange": false,
+  "Interpolation": "LINEAR",
+  "Nodes": [
+    [0.122, 0.467, 0.706, 1.0, 0.0, 0.05],
+    [1.0, 0.498, 0.055, 1.0, 0.5, 0.05],
+    [0.173, 0.627, 0.173, 1.0, 1.0, 0.05]
+  ],
+  "TextureSize": 3,
+  "ValueRange": [
+    0.0,
+    2.0
+  ]
+}]=])
